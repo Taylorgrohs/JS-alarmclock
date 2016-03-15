acb4c513285ddc5b5cb4dc30d0b5feacce6f376a
Jay Whang and Taylor Grohs <taylor@>
2016-03-15 12:34:44 -0700

    add working alarm function

--- js/browser-interface.js
+++ js/browser-interface.js
@@ -1,8 +1,36 @@
 var update = require('./../js/clock-interface.js').update;
+// var setAlarm = require('./../js/alarm-interface.js').setAlarm;
+// var alarmChecker = require('./../js/alarm-interface.js').alarmChecker;
 
-var datetime = null,
-        date = null;
-
+var myVar;
+setAlarm = function(input) {
+  var save = input;
+  var currentTime = moment().format('h:mm');
+  if(save.toString() === currentTime.toString())
+  {
+    return $(".jumbotron").css("background-color", "red");
+  }
+}
+alarmChecker = function (input) {
+    myVar = setInterval(setAlarm(input), 1000);
+}
 $(document).ready(function(){
+  var input = null;
+
     setInterval(update, 1000);
-});
+    $('#alarm').submit(function(event){
+      event.preventDefault();
+      input = $('#set-alarm').val();
+      alarm = setAlarm(input);
+
+      $('#alarm-set').append("<p>"+input+"</p>");
+      setInterval(function(){
+        var currentTime = moment().format('h:mm');
+        if(input === currentTime)
+        {
+          return $(".jumbotron").css("background-color", "red");
+        }
+      }, 1000);
+    });
+
+  });
